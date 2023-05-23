ebd83e27593ea07a74d1de21ee470cca4133eaa1
AChang0402 <alexchang.0402@gmail.com>
2023-05-22 21:01:07 -0700

    2023.05.22

---
+++ w4d1.js
@@ -0,0 +1,110 @@
+/**
+ * Class to represent a stack using an array to store the stacked items.
+ * Follows a LIFO (Last In First Out) order where new items are stacked on
+ * top (back of array) and removed items are removed from the top / back.
+ */
+class Stack {
+    /**
+     * The constructor is executed when instantiating a new Stack() to construct
+     * a new instance.
+     * @returns {Stack} This new Stack instance is returned without having to
+     *    explicitly write 'return' (implicit return).
+     */
+    constructor() {
+        this.items = [];
+    }
+
+    /**
+     * Adds a new given item to the top / back of this stack.
+     * - Time: O(1) constant.
+     * - Space: O(1) constant.
+     * @param {any} item The new item to be added to the top / back.
+     * @returns {number} The new length of this stack.
+     */
+    push(item) {
+        this.items.push(item);
+        return this.items.length;
+    }
+
+    /**
+     * Removes the top / last item from this stack.
+     * - Time: O(1) constant.
+     * - Space: O(1) constant.
+     * @returns {any} The removed item or undefined if this stack was empty.
+     */
+    pop() {
+        if (this.isEmpty()){
+            return undefined;
+        }
+        return this.items.pop();
+    }
+
+    /**
+     * Retrieves the top / last item from this stack without removing it.
+     * - Time: O(1) constant.
+     * - Space: O(1) constant.
+     * @returns {any} The top / last item of this stack.
+     */
+    peek() {
+        if (this.isEmpty){
+            return null
+        }
+        return this.items[this.items.length-1];
+    }
+
+    /**
+     * Returns whether or not this stack is empty.
+     * - Time: O(1) constant.
+     * - Space: O(1) constant.
+     * @returns {boolean}
+     */
+    isEmpty() {
+        if (this.items.length > 0){
+            return false;
+        }
+        else {
+            return true;
+        }
+    }
+
+    /**
+     * Returns the size of this stack.
+     * - Time: O(1) constant.
+     * - Space: O(1) constant.
+     * @returns {number} The length.
+     */
+    size() {
+        return this.items.length;
+    }
+}
+
+
+//EXTRA - Try re-creating a stack using a linked list data structure
+
+class StackNode {
+    constructor(data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+class LinkedListStack {
+    constructor() {
+        this.head = null;
+    }
+}
+
+let testStack = new Stack();
+console.log(testStack.isEmpty())
+testStack.push(5);
+testStack.push(7)
+testStack.push(3)
+testStack.push(2)
+testStack.push(8)
+console.log(testStack.isEmpty())
+console.log(testStack.items)
+console.log(testStack.push(9))
+console.log(testStack.items)
+console.log(testStack.pop())
+console.log(testStack.items)
+console.log(testStack.size())
