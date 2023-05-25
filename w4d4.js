5ec9ff95c0214c625fa8eca9836bad1668b37fdf
AChang0402 <alexchang.0402@gmail.com>
2023-05-25 09:28:42 -0700

    2023.05.25

---
+++ w4d4.js
@@ -0,0 +1,289 @@
+/**
+ * Class to represent a queue using an array to store the queued items.
+ * Follows a FIFO (First In First Out) order where new items are added to the
+ * back and items are removed from the front.
+ */
+class Queue {
+    constructor() {
+        this.items = [];
+    }
+
+    /**
+     * Determines whether the sum of the left half of the queue items is equal to
+     * the sum of the right half. Avoid indexing the queue items directly via
+     * bracket notation, use the queue methods instead for practice.
+     * Use no extra array or objects.
+     * The queue should be returned to it's original order when done.
+     * - Time: O(?).
+     * - Space: O(?).
+     * @returns {boolean} Whether the sum of the left and right halves is equal.
+     */
+    isSumOfHalvesEqual() {
+        let sum1 = 0;
+        let sum2 = 0;
+        let count = 0;
+        const half = Math.floor(this.size()/2);
+        console.log("This is our half " + half);
+        while(count < this.size()){
+            count++;
+            const tempVal = this.dequeue();
+            this.enqueue(tempVal)
+            if(this.size() % 2 == 1){
+                if(count == half+1){
+                    continue;
+                }
+            }
+            if(count <= half){
+                sum1 += tempVal;
+                console.log("1: " + sum1);
+            }
+            else{
+                sum2 += tempVal;
+                console.log("2: " + sum2);
+            }
+        }
+        return sum1 == sum2
+    }
+
+    /**
+     * Compares this queue to another given queue to see if they are equal.
+     * Avoid indexing the queue items directly via bracket notation, use the
+     * queue methods instead for practice.
+     * Use no extra array or objects.
+     * The queues should be returned to their original order when done.
+     * - Time: O(n^2) quadratic, n = queue length. Quadratic due to dequeue on an
+     *     array queue being O(n).
+     * - Space: O(1) constant.
+     * @param {Queue} q2 The queue to be compared against this queue.
+     * @returns {boolean} Whether all the items of the two queues are equal and
+     *    in the same order.
+     */
+    compareQueues(q2) {
+        if (this.size() !== q2.size()) {
+            return false;
+        }
+        let count = 0;
+        let isEqual = true;
+        const len = this.size();
+
+        while (count < len) {
+            const dequeued1 = this.dequeue();
+            const dequeued2 = q2.dequeue();
+
+            if (dequeued1 !== dequeued2) {
+                isEqual = false;
+            }
+
+            this.enqueue(dequeued1);
+            q2.enqueue(dequeued2);
+            count++;
+        }
+        return isEqual;
+    }
+
+    /**
+     * Determines if the queue is a palindrome (same items forward and backwards).
+     * Avoid indexing queue items directly via bracket notation, instead use the
+     * queue methods for practice.
+     * Use only 1 stack as additional storage, no other arrays or objects.
+     * The queue should be returned to its original order when done.
+     * - Time: O(n^2) quadratic, n = queue length. Quadratic due to dequeue on an
+     *     array queue being O(n).
+     * - Space: O(n) from the stack being used to store the items again.
+     * @returns {boolean}
+     */
+    isPalindrome() {
+        let isPalin = true;
+        const stack = new Stack(),
+            len = this.size();
+
+        for (let i = 0; i < len; i++) {
+            let dequeued = this.dequeue();
+            stack.push(dequeued);
+            // add it back so the queue items and order is restored at the end
+            this.enqueue(dequeued);
+        }
+
+        for (let i = 0; i < len; i++) {
+            let dequeued = this.dequeue();
+            let popped = stack.pop();
+
+            if (popped !== dequeued) {
+                isPalin = false;
+            }
+
+            // add it back so the queue items and order is restored at the end
+            this.enqueue(dequeued);
+        }
+        return isPalin;
+    }
+
+    /**
+     * Adds a new given item to the back of this queue.
+     * - Time: O(1) constant.
+     * - Space: O(1) constant.
+     * @param {any} item The new item to add to the back.
+     * @returns {number} The new size of this queue.
+     */
+    enqueue(item) {
+        this.items.push(item);
+        return this.size();
+    }
+
+    /**
+     * Removes and returns the first item of this queue.
+     * - Time: O(n) linear, due to having to shift all the remaining items to
+     *    the left after removing first elem.
+     * - Space: O(1) constant.
+     * @returns {any} The first item or undefined if empty.
+     */
+    dequeue() {
+        return this.items.shift();
+    }
+
+    /**
+     * Retrieves the first item without removing it.
+     * - Time: O(1) constant.
+     * - Space: O(1) constant.
+     * @returns {any} The first item or undefined if empty.
+     */
+    front() {
+        return this.items[0];
+    }
+
+    /**
+     * Returns whether or not this queue is empty.
+     * - Time: O(1) constant.
+     * - Space: O(1) constant.
+     * @returns {boolean}
+     */
+    isEmpty() {
+        return this.items.length === 0;
+    }
+
+    /**
+     * Retrieves the size of this queue.
+     * - Time: O(1) constant.
+     * - Space: O(1) constant.
+     * @returns {number} The length.
+     */
+    size() {
+        return this.items.length;
+    }
+}
+
+/* Rebuild the above class using a linked list instead of an array. */
+
+class QueueNode {
+    constructor(data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+class LinkedListQueue {
+    constructor() {
+        this.head = null;
+        this.tail = null;
+        this.size = 0;
+    }
+
+    /**
+     * - Time: O(1) constant.
+     * - Space: O(1) constant.
+     * @returns {boolean} Indicates if the list is empty.
+     */
+    isEmpty() {
+        return this.head === null;
+    }
+
+    /**
+     * Adds a given val to the back of the queue.
+     * - Time: O(1) constant.
+     * - Space: O(1) constant.
+     * @param {any} val
+     * @returns {number} The new size of the queue.
+     */
+    enqueue(val) {
+        const newTail = new QueueNode(val);
+
+        if (this.isEmpty()) {
+            this.head = newTail;
+            this.tail = newTail;
+        } else {
+            this.tail.next = newTail;
+            this.tail = newTail;
+        }
+        // pre-increment so the new size is returned otherwise old size is returned
+        return ++this.size;
+    }
+
+    /**
+     * - Time: O(1) constant.
+     * - Space: O(1) constant.
+     * @returns {any} The removed item.
+     */
+    dequeue() {
+        // remove head
+        if (!this.head) {
+            return null;
+        }
+
+        const dequeued = this.head;
+        this.head = this.head.next;
+
+        if (this.head === null) {
+            this.tail = null;
+        }
+
+        this.size--;
+        return dequeued.data;
+    }
+
+    /**
+     * Retrieves the first item without removing it.
+     * - Time: O(1) constant.
+     * - Space: O(1) constant.
+     * @returns {any} The first item.
+     */
+    front() {
+        return this.head ? this.head.data : null;
+    }
+
+    /**
+     * Determines if the given item is in the queue.
+     * - Time: O(n) linear.
+     * - Space: O(1) constant.
+     * @param {any} searchVal
+     * @returns {boolean}
+     */
+    contains(searchVal) {
+        let runner = this.head;
+
+        while (runner) {
+            if (runner.searchVal === searchVal) return true;
+            runner = runner.next;
+        }
+        return false;
+    }
+
+    /**
+     * Enqueues each of the given items.
+     * - Time: O(n) linear since enqueue is O(1), n = vals.length.
+     * - Space: O(1) constant.
+     * @param {Array<any>} vals
+     */
+    seed(vals) {
+        vals.forEach((val) => this.enqueue(val));
+    }
+}
+const queueFour = new Queue;
+queueFour.enqueue(1)
+queueFour.enqueue(3)
+queueFour.enqueue(2)
+queueFour.enqueue(5)
+queueFour.enqueue(3)
+queueFour.enqueue(1)
+queueFour.enqueue(2)
+
+console.log(queueFour.isSumOfHalvesEqual())
